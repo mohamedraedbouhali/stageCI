433522f4ee33cbd0990fbbe19890f35a5734c5ed
raed <raedbouhali@gmail.com>
2025-08-28 11:06:02 +0100

    First commit

---
+++ CI.py
@@ -0,0 +1,12 @@
+# A simple function to be tested
+def greet(name):
+    """
+    Returns a greeting message.
+    """
+    return f"Hello, {name}!"
+
+def add_numbers(a, b):
+    """
+    Adds two numbers and returns the sum.
+    """
+    return a + b
